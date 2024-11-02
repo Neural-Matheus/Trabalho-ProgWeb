986009f6034ddfeb10cc094f37c3398c70ee1ec9
Neural-Matheus <vinicius.matheus@ufms.br>
2024-11-02 18:19:31 -0400

    feat: páginas HTML iniciais

---
+++ src/model/js/pagina_recuperar_senha.js
@@ -0,0 +1,4 @@
+document.querySelector(".form").addEventListener("submit", function(event) {
+  event.preventDefault();
+  alert("Instruções para recuperação de senha foram enviadas para o seu email.");
+});
